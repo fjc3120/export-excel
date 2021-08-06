2d32cc41df2d567d4f5a357bddf640b9e155d17b
冯建超 <jianchao.feng@hand-china.com>
2021-08-06 15:34:58 +0800

    更新

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
  * @Copyright: Copyright (c) 2018, Hand
  * 前端导出组件的配置信息option相关参考请看下文
  * https://www.cnblogs.com/liuxianan/p/js-excel.html
- * obj为一个对象，包含三个元素: Array-dataList(表格数据)  Object-option(配置信息) Array-columnsList(列头信息)
+ * obj为一个对象，包含四个元素: Array-dataList(表格数据)  Object-option(配置信息) Array-columnsList(列头信息) String-title(文件名/标题名)优先级最高
  * let columnsList = [
       { name: '第一列name', code : '第一列code'},
       { name: '第二列name', code : '第二列code'},
@@ -20,4 +20,68 @@
         {'第二行第一列code': '第二行第一列value', '第二行第二列code': '第二行第二列value'}, // 第二行数据
         ...
     ];
+    let option = {
+        title: 'excel表单' || '未命名', // excel文件标题名
+        width: 150, // 单元格宽度
+        fontSize: 12, // 字体大小-列头
+        fontSizeTitle: 14, // 字体大小-标题
+        fontSizeList: 10, // 字体大小-表格
+        fontBold: true, // 列头文字是否加粗
+        fillColor: 'B7DEEA', // 列头单元格背景色(颜色编码没有#)
+        alignmentVertical: 'center', // 垂直
+        alignmentHorizontal: 'center', // 水平
+        styleGroup: [ // 批量单元格自定制样式
+            {
+                cells: ['A3', 'A11', 'A19', 'A27'],
+                style: {
+                    font: { sz: 10, color: { rgb: '4fd2db' } },
+                    alignment: { vertical: 'center', horizontal: 'bottom', wrapText: 'true' },
+                },
+            },
+            {
+                cells: ['B5'],
+                style: {
+                    font: { sz: 10, color: { rgb: 'cccccc' } },
+                    alignment: { vertical: 'center', horizontal: 'bottom', wrapText: 'true' },
+                },
+            },
+        ],
+        styleRow: [ // 批量单元格——以一行为维度，自定制样式
+            {
+                cells: ['3', '5'],
+                style: {
+                    font: { sz: 10, color: { rgb: 'cccccc' } },
+                    alignment: { vertical: 'center', horizontal: 'bottom', wrapText: 'true' },
+                },
+            },
+            {
+                cells: ['6'],
+                style: {
+                    font: { sz: 14, color: { rgb: 'cccccc' } },
+                    alignment: { vertical: 'center', horizontal: 'bottom', wrapText: 'true' },
+                },
+            },
+        ],
+        widthColums: [ // 以列为维度设置单元格宽度
+            {
+                cells: [3, 5],
+                width: 100,
+            },
+            {
+                cells: [1],
+                width: 200,
+            },
+        ],
+        merges: [ // 单元格合并，s-e 代表区域 c-r 代表列-行的索引
+            {
+                s: { c: 0, r: 2 },
+                e: { c: 0, r: 5 },
+            },
+            {
+                s: { c: 0, r: 10 },
+                e: { c: 3, r: 10 },
+            },
+        ],
+    };
+    let title = 'excel表单' || '未命名';
  */
--- index.js
+++ index.js
@@ -6,7 +6,7 @@
  * @Copyright: Copyright (c) 2018, Hand
  * 前端导出组件的配置信息option相关参考请看下文
  * https://www.cnblogs.com/liuxianan/p/js-excel.html
- * obj为一个对象，包含三个元素: Array-dataList(表格数据)  Object-option(配置信息) Array-columnsList(列头信息)
+ * obj为一个对象，包含四个元素: Array-dataList(表格数据)  Object-option(配置信息) Array-columnsList(列头信息) String-title(文件名/标题名)优先级最高
  * let columnsList = [
       { name: '第一列name', code : '第一列code'},
       { name: '第二列name', code : '第二列code'},
@@ -20,19 +20,95 @@
     let option = {
         title: 'excel表单' || '未命名', // excel文件标题名
         width: 150, // 单元格宽度
-        fontSize: 12, // 字体大小
-        fontBold: true, // 文字是否加粗
-        fillColor: 'B7DEEA', // 单元格背景色(颜色编码没有#)
+        fontSize: 12, // 字体大小-列头
+        fontSizeTitle: 14, // 字体大小-标题
+        fontSizeList: 10, // 字体大小-表格
+        fontBold: true, // 列头文字是否加粗
+        fillColor: 'B7DEEA', // 列头单元格背景色(颜色编码没有#)
         alignmentVertical: 'center', // 垂直
         alignmentHorizontal: 'center', // 水平
+        styleGroup: [ // 批量单元格自定制样式
+        {
+          cells: ['A3', 'A11', 'A19', 'A27'],
+          style: {
+            font: { sz: 10, color: { rgb: '4fd2db' } },
+            alignment: { vertical: 'center', horizontal: 'bottom', wrapText: 'true' },
+          },
+        },
+        {
+          cells: ['B5'],
+          style: {
+            font: { sz: 10, color: { rgb: 'cccccc' } },
+            alignment: { vertical: 'center', horizontal: 'bottom', wrapText: 'true' },
+          },
+        },
+      ],
+      styleRow: [ // 批量单元格——以一行为维度，自定制样式
+        {
+          cells: ['3', '5'],
+          style: {
+            font: { sz: 10, color: { rgb: 'cccccc' } },
+            alignment: { vertical: 'center', horizontal: 'bottom', wrapText: 'true' },
+          },
+        },
+        {
+          cells: ['6'],
+          style: {
+            font: { sz: 14, color: { rgb: 'cccccc' } },
+            alignment: { vertical: 'center', horizontal: 'bottom', wrapText: 'true' },
+          },
+        },
+      ],
+      widthColums: [ // 以列为维度设置单元格宽度
+        {
+          cells: [3, 5],
+          width: 100,
+        },
+        {
+          cells: [1],
+          width: 200,
+        },
+      ],
+      merges: [ // 单元格合并，s-e 代表区域 c-r 代表列-行的索引
+        {
+          s: { c: 0, r: 2 },
+          e: { c: 0, r: 5 },
+        },
+        {
+          s: { c: 0, r: 10 },
+          e: { c: 3, r: 10 },
+        },
+      ],
     };
+    let title = 'excel表单' || '未命名';
  */
     import XLSX from './xlsx-style';
     function ExportExcel(obj={}) {
       // 点击导出按钮
       function handleExportData() {
         if (Object.keys(obj).length) {
-          let { dataList = [], option = {}, columnsList = [] } = obj;
+          let {
+            dataList = [],
+            option = {},
+            columnsList = [],
+            title = '',
+          } = obj;
+          option.title = option.title || '未命名';
+          option.width= option.width || 150;
+          option.fontSize= option.fontSize || 12;
+          option.fontSizeTitle= option.fontSizeTitle || 14;
+          option.fontSizeList= option.fontSizeList || 10;
+          option.fontBold= option.fontBold || true;
+          option.fillColor= option.fillColor || 'B7DEEA';
+          option.alignmentVertical= option.alignmentVertical || 'center';
+          option.alignmentHorizontal= option.alignmentHorizontal || 'center';
+          option.styleGroup= option.styleGroup || [];
+          option.styleRow= option.styleRow || [];
+          option.widthColums= option.widthColums || [];
+          option.merges= option.merges || [];
+          if (title) {
+            option.title = title;
+          }
           exportRun(dataList, option, columnsList); // 数据-配置信息-列头信息
         }
       }
@@ -41,7 +117,7 @@
       function exportRun(data, option, columns) {
         if (data.length) {
           let dataJson = [];
-          let len = columns.length;
+          let columnsLen = columns.length;
           data.forEach(item => {
             let obj = {};
             columns.forEach(i => {
@@ -51,7 +127,7 @@
           });
           // 配置文件类型
           const wopts = { bookType: 'xlsx', bookSST: true, type: 'binary', cellStyles: true };
-          downloadExl(dataJson, wopts, option, len);
+          downloadExl(dataJson, wopts, option, columnsLen);
         }
       }
     
@@ -100,7 +176,7 @@
               (tmpdata[v.position] = {
                 v: v.v,
                 s: {
-                  font: { sz: 10 },
+                  font: { sz: option.fontSizeList },
                   alignment: { vertical: 'center', horizontal: 'center', wrapText: 'true' },
                   border: borderAll,
                 },
@@ -110,7 +186,6 @@
         tmpdata.A1 = { v: option.title }; // A1-A4区域的内容
         outputPos = ['A1'].concat(outputPos);
         // 对所有单元格列的样式批量处理
-        console.log(tmpdata)
         Object.keys(tmpdata).forEach(item => {
           if (item.charAt(item.length - 1) === '2' && isNaN(parseFloat(item.charAt(item.length - 2)))) {
             tmpdata[item].s = {
@@ -123,10 +198,37 @@
         });
         // ======================在此处对某一列单元格样式进行单独处理==============================
         tmpdata.A1.s = {
-          font: { sz: 14, bold: true },
+          font: { sz: option.fontSizeTitle, bold: true },
           border: borderAll,
           alignment: { vertical: 'center', horizontal: 'center' },
         };
+        // 列级别样式修改
+        if (option.styleRow && option.styleRow.length) {
+          option.styleRow.forEach(item => {
+            item.style.border = borderAll;
+            item.cells.forEach(i => {
+              Object.keys(tmpdata).forEach(j => {
+                let num = /(\d+(\.\d+)?)/.exec(j);
+                if (num && num.length > 0) {
+                    num = num[0];
+                }
+                if (i === num) {
+                  tmpdata[j].s = item.style;
+                }
+              });
+            });
+          });
+        }
+
+        // 单元格批量样式修改
+        if (option.styleGroup && option.styleGroup.length) {
+          option.styleGroup.forEach(item => {
+            item.style.border = borderAll;
+            item.cells.forEach(i => {
+              tmpdata[i].s = item.style;
+            });
+          });
+        }
         // s-e 代表区域 c-r 代表列-行的索引
         let mergesLen = columnsLen - 1;
         tmpdata['!merges'] = [
@@ -135,9 +237,25 @@
             e: { c: mergesLen, r: 0 },
           },
         ]; // <====合并单元格
+
+        // 设置单元格合并
+        if (option.merges && option.merges.length) {
+          tmpdata['!merges'] = tmpdata['!merges'].concat(option.merges);
+        }
+
         let dataArrWidth = [];
         for (let i = 0; i < columnsLen + 1; i++) {
-          dataArrWidth.push({ wpx: option.width });
+          dataArrWidth.push({ wpx: option.width || 150 });
+        }
+
+        // 例 dataArrWidth[3].wpx = 130;
+        // 以列为维度，设置单元格宽度
+        if (option.widthColums && option.widthColums.length) {
+          option.widthColums.forEach(item => {
+            item.cells.forEach(i => {
+              dataArrWidth[i].wpx = item.width;
+            });
+          });
         }
         tmpdata['!cols'] = dataArrWidth; // <====设置一列宽度， 代表20列都是300宽
         const tmpWB = {
